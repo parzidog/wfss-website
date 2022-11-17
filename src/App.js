e57de0906769968de861aeedca3488b8fcedbdf0
Kenenth Schaefer <kenneth.schaefer0@gmail.com>
2022-11-17 15:07:41 -0600

    Add Navbar, Home and Location pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/Main";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Location from "./components/Location";
+import Pricing from "./components/Pricing";
+import NotFound from "./components/NotFound";
+
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route element={<Main />} >
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="location" element={<Location />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id='contact'>
+      <div id='description'>
+        <h1>Contact Us</h1>
+        <p>
+          Feel free to call us at (940) 386-1146 with any questions you may have, or to start your next great rental experience today! You can also contact us using this online form if that is more convenient, and a member of our staff will get back to you as soon as possible.</p>
+      </div>
+      <div id='contact-form'>
+        <form action="">
+          <input type="text" placeholder='Name' />
+          <input type="email" placeholder='Email' />
+          <input type="tel" placeholder='Phone' />
+          <textarea name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
