a645dbccd94a8cd0347b271a1a56b6cdd3c9c9ea
Kenenth Schaefer <kenneth.schaefer0@gmail.com>
2022-11-21 11:27:35 -0600

    Add styling and nmp packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
-  mode: "development",
   entry: ["./src/index.js"],
+
   output: {
     path: __dirname + "/public",
     filename: "bundle.js",
@@ -12,16 +14,24 @@ module.exports = {
       directory: __dirname + "/public",
     },
   },
+
   module: {
     rules: [
       {
         test: /jsx?$/,
         exclude: /node_modules/,
+
         loader: "babel-loader",
+
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
+  plugins: [new Dotenv({ systemvars: true })],
 };
